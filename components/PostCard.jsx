fd0f26004eec9afd1b6369bc7886af97fcff4a8b
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2021-11-10 16:04:08 +0700

    Get post photo

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -4,9 +4,14 @@ import Link from 'next/link';
 
 function PostCard({ post }) {
   return (
-    <div>
-      {post.title}
-      {post.excerpt}
+    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+        <img
+          src={post.featuredimage[0].url}
+          alt={post.title}
+          className="object-top absolute h-80 w-full object-cocver shadow-lg rounded-t-lg lg:rounded-lg"
+        />
+      </div>
     </div>
   );
 }
