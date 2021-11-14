6c8831dd2cc11c5a561267698a7de2d9beb2de4f
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2021-11-14 14:58:35 +0700

    Add categories

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 
-const categories = [
-  { name: 'React', slug: 'react' },
-  { name: 'Web Dev', slug: 'web-dev' },
-];
-function Header() {
+import { getCategories } from '../services';
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
@@ -28,6 +32,6 @@ function Header() {
       </div>
     </div>
   );
-}
+};
 
 export default Header;
