15f5ef5dfa43f6207624c9e3ae9d5174c352e47d
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2021-11-16 15:40:25 +0700

    Add element in comment form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -9,7 +9,29 @@ const CommentsForm = ({ slug }) => {
   const emailEl = useRef();
   const storeDataEl = useRef();
 
-  const handleCommentSubmission = () => {};
+  const handleCommentSubmission = () => {
+    setError(false);
+
+    const { value: comment } = commentEl.current;
+    const { value: name } = nameEl.current;
+    const { value: email } = emailEl.current;
+    const { checked: storeData } = storeDataEl.current;
+
+    if (!comment || !name || !email) {
+      setError(true);
+      return;
+    }
+
+    const commentObj = { name, email, comment, slug };
+
+    if (storeData) {
+      localStorage.setItem('name', name);
+      localStorage.setItem('email', email);
+    } else {
+      localStorage.removeItem('name', name);
+      localStorage.removeItem('email', email);
+    }
+  };
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
@@ -38,9 +60,37 @@ const CommentsForm = ({ slug }) => {
           name="email"
         />
       </div>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <div>
+          <input
+            type="checkbox"
+            ref={storeDataEl.el}
+            id="storeData"
+            name="storeData"
+            value="true"
+          />
+          <label
+            className="text-gray-500 cursor-pointer ml-2"
+            htmlFor="storeData"
+          >
+            Save my e-mail and name for the next time I comment.
+          </label>
+        </div>
+      </div>
       {error && <p className="text-xs text-red-500">All field are required.</p>}
       <div className="mt-8">
-        <button type="button" onClick={handleCommentSubmission}></button>
+        <button
+          type="button"
+          onClick={handleCommentSubmission}
+          className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer"
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className="text-xl float-right font-semibold mt-3 text-green-500">
+            Comment submitted for review
+          </span>
+        )}
       </div>
     </div>
   );
