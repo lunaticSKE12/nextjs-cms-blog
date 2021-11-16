e25c611915cd2b9e0417aa8f76a07d28e1b5edbc
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2021-11-16 22:14:24 +0700

    Try post comment

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-
+import { submitComment } from '../services';
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
@@ -8,6 +8,11 @@ const CommentsForm = ({ slug }) => {
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name');
+    emailEl.current.value = window.localStorage.getItem('email');
+  }, []);
 
   const handleCommentSubmission = () => {
     setError(false);
@@ -25,17 +30,27 @@ const CommentsForm = ({ slug }) => {
     const commentObj = { name, email, comment, slug };
 
     if (storeData) {
-      localStorage.setItem('name', name);
-      localStorage.setItem('email', email);
+      window.localStorage.setItem('name', name);
+      window.localStorage.setItem('email', email);
     } else {
-      localStorage.removeItem('name', name);
-      localStorage.removeItem('email', email);
+      window.localStorage.removeItem('name', name);
+      window.localStorage.removeItem('email', email);
     }
+
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMessage(true);
+
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    });
   };
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">CommentsForm</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Leave a reply
+      </h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
           ref={commentEl}
@@ -64,7 +79,7 @@ const CommentsForm = ({ slug }) => {
         <div>
           <input
             type="checkbox"
-            ref={storeDataEl.el}
+            ref={storeDataEl}
             id="storeData"
             name="storeData"
             value="true"
